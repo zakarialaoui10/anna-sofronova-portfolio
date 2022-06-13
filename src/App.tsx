8a1c39f9a70287930aab6660c8f6f25705ef4914
Anna <anysofronova@yandex.ru>
2022-06-13 19:09:44 +0300

    Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <>
       <Header />
+      <Navigation />
     </>
   );
 }
