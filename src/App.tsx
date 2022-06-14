a6bf6340c70ee4ab69669766a730bfe548c4dce3
Anna <anysofronova@yandex.ru>
2022-06-14 11:13:06 +0300

    About component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Header />
       <Navigation />
+      <About />
     </>
   );
 }
