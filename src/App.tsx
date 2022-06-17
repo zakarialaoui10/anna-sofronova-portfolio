c033f639d8b0011ea5299db7cff552c2ecdd0581
Anna <anysofronova@yandex.ru>
2022-06-17 17:31:10 +0300

    Contact

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation/Navigation";
 import About from "./components/About/About";
 import Education from "./components/Education/Education";
 import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About />
       <Education />
       <Skills />
+      <Contact />
     </>
   );
 }
