85c84322c39a0e167ec0e61ec09c1c2392c83eec
Anna <anysofronova@yandex.ru>
2022-06-20 17:47:22 +0300

    Portfolio Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation/Navigation";
 import About from "./components/About/About";
 import Education from "./components/Education/Education";
 import Skills from "./components/Skills/Skills";
+import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Education />
       <Skills />
+      <Portfolio />
       <Contact />
     </>
   );
---
+++ src/components/Portfolio/PortfolioItem/PortfolioItem.tsx
@@ -0,0 +1,30 @@
+import styles from "./PortfolioItem.module.scss";
+import { Item } from "../Portfolio";
+
+const PortfolioItem = ({ img, name, tags, github, link }: Item) => {
+  return (
+    <div className={styles.item} data-img={img}>
+      <div className={styles.image}>
+        <img src={img} alt="Portfolio Item" />
+      </div>
+      <h3>{name}</h3>
+      <div className={styles.links}>
+        <a href={link} target="_blank" rel="noreferrer">
+          DEMO
+        </a>
+        <a href={github} target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+      </div>
+      <div className={styles.tags}>
+        {tags.map((i, idx) => (
+          <div className={styles.tag} key={idx}>
+            {i}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioItem;
